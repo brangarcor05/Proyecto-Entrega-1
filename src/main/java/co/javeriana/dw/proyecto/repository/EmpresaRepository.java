bd81d843aa65c18b5c9fcf6a515ea31b6b9f0f30
brangarcor05 <brandon.garcrod1980@gmail.com>
2026-08-31 20:22:19 +0000

    fix: correciones en repositorio empresa y application

--- src/main/java/co/javeriana/dw/proyecto/repository/EmpresaRepository.java
+++ src/main/java/co/javeriana/dw/proyecto/repository/EmpresaRepository.java
@@ -1,15 +1,11 @@
 package co.javeriana.dw.proyecto.repository;
 
-import co.javeriana.dw.proyecto.entidad.Empresa;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import co.javeriana.dw.proyecto.entidad.Empresa;
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
 
     List<Empresa> findAllByOrderByIdAsc();
-
-<<<<<<< HEAD
 }
-
-=======
->>>>>>> 05e26fa566eab0cb8c63197e7bc337b389f85462
