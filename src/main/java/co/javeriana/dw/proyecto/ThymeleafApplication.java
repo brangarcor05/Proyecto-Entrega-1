664c01c2726ffb044f9effbbc9e7466f0beea057
brangarcor05 <brandon.garcrod1980@gmail.com>
2026-08-31 21:27:10 -0500

    fix: conexion a la base de datos

--- src/main/java/co/javeriana/dw/proyecto/ThymeleafApplication.java
+++ src/main/java/co/javeriana/dw/proyecto/ThymeleafApplication.java
@@ -2,9 +2,9 @@ package co.javeriana.dw.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
+@SpringBootApplication
 public class ThymeleafApplication {
 
 	public static void main(String[] args) {
